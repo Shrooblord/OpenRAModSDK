8dd802b1ef69293c276aa136b94c56db124ea853
Shrooblord <shrooblord@thejaycode.com>
2025-02-04 23:00:22 +0100

    [wip] add MobileRefinery Trait
    Attempts creating a new C# Trait class and adding that to a unit. However, the TraitInfo for this new Trait currently can't be found when you run the game, causing a crash. I'm sure it has something to do with getting the engine to import the directory properly, but I haven't figured out yet where I'm messing this up.

---
+++ OpenRA.Mods.Shroobs/Traits/MobileRefinery.cs
@@ -0,0 +1,28 @@
+﻿#pragma warning disable SA1124
+#region Copyright & License Information
+#pragma warning restore SA1124
+/*
+ * Copyright (c) The OpenRA Developers and Contributors
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+namespace OpenRA.Mods.Shroobs.Traits
+{
+    using OpenRA.Mods.Common.Traits;
+    using OpenRA.Traits;
+
+    public class MobileRefineryInfo : RefineryInfo, Requires<MobileInfo>
+    {
+        public override object Create(ActorInitializer init) { return new MobileRefinery(init.Self, this); }
+    }
+
+    public class MobileRefinery(Actor self, MobileRefineryInfo info) : Refinery(self, info)
+    {
+        readonly Actor self = self;
+    }
+}
