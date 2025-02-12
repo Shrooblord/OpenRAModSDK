1b5ff6d0639812b5d3265b844ac909f64e4e2921
Shrooblord <shrooblord@thejaycode.com>
2025-02-12 14:40:39 +0100

    add Collector unit that harvests Tiberium and brings it to the Extractor

--- OpenRA.Mods.Shroobs/Traits/MobileRefinery.cs
+++ OpenRA.Mods.Shroobs/Traits/MobileRefinery.cs
@@ -24,7 +24,7 @@ namespace OpenRA.Mods.Shroobs.Traits
     public class MobileRefinery(Actor self, MobileRefineryInfo info) : Refinery(self, info)
     {
         readonly Actor self = self;
-    }
 
-    // @todo: create AllowSelfDock Trait: allows for a MobileRefinery that is also a Harvester to Dock with itself
+        // @todo: create AllowSelfDock Trait: allows for a MobileRefinery that is also a Harvester to Dock with itself
+    }
 }
